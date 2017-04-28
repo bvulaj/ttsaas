4cf15c975e0d7cef8a928aa9e36226b4edde398d
Brandon Vulaj <brandon.vulaj@gmail.com>
2017-04-28 11:55:41 -0400

    Added basic index page for testing

--- ttsaas-webservice/src/main/java/com/redhat/ttsaas/TextToSpeechController.java
+++ ttsaas-webservice/src/main/java/com/redhat/ttsaas/TextToSpeechController.java
@@ -5,17 +5,27 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+import com.redhat.ttsaas.model.TextToSynthesize;
+
+@Controller("/")
 public class TextToSpeechController {
 
+	@GetMapping
+	public String home(Model model) {
+		model.addAttribute("tts", new TextToSynthesize());
+		return "index";
+	}
+
 	@PostMapping("/tts")
-	public @ResponseBody void readText(@RequestBody String text, HttpServletResponse response) throws IOException {
+	public @ResponseBody void readText(@ModelAttribute TextToSynthesize tts, HttpServletResponse response) throws IOException {
 		// do the tts
-		byte[] speechBytes = text.getBytes();
+		byte[] speechBytes = tts.getText().getBytes();
 
 		response.getOutputStream().write(speechBytes);
 	}
